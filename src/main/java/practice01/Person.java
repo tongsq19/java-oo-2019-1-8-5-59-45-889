c05a626a74ec042d0f18b9ad9b3a52bba2b4f4c2
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 06:51:30 +0800

    Pass practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,24 @@
 package practice01;
 
 public class Person {
+    private int age;
+    private String name;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + name +". I am " + age + " years old.";
+    }
 }
+
