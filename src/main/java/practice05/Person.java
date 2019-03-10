4c2216c8d30ddd01e291842094702c384fe9e8f8
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 10:29:39 +0800

    Pass Practice06

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,29 @@
 package practice05;
 
 public class Person {
+    private int age;
+    private String name;
+
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return basicIntroduce();
+    }
+
+    protected String basicIntroduce() {
+        return "My name is " + name +". I am " + age + " years old.";
+    }
 }
+
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        klass = -1;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(klass == -1) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach Class "+ klass+".";
+    }
+
 }
