845c2de9884771026e1256f5a5a1d7b8c9b5f857
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 11:26:40 +0800

    Pass Practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        leader = student;
+    }
 }
